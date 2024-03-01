c2d5251dfabd30cc7a421645f33cc7fc94e68ce3
Shawn Ferro <shawnf@cybage.com>
2024-03-01 18:29:53 +0530

    FUR-7-Registration-Frontend Alterations in css of registration

--- Frontend/src/app/registration/registration.module.ts
+++ Frontend/src/app/registration/registration.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegistrationRoutingModule } from './registration-routing.module';
 import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../services/auth.service';
 import { HttpClientModule } from '@angular/common/http';
@@ -10,8 +9,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    RegisterComponent,
-    LoginComponent
+    RegisterComponent
   ],
   imports: [
     CommonModule,
@@ -20,8 +18,7 @@ import { HttpClientModule } from '@angular/common/http';
     HttpClientModule
   ],
   exports:[
-    RegisterComponent,
-    LoginComponent
+    RegisterComponent
   ],
   providers:[
     AuthService
