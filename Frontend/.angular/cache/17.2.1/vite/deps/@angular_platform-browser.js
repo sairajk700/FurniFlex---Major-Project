36274c772675401882ef1fa29770d8a10432847c
Shawn Ferro <shawnf@cybage.com>
2024-03-04 16:34:23 +0530

    FUR-12-Wishlist Created wishlist and did all the css and designing of the page

--- Frontend/.angular/cache/17.2.1/vite/deps/@angular_platform-browser.js
+++ Frontend/.angular/cache/17.2.1/vite/deps/@angular_platform-browser.js
@@ -28,7 +28,6 @@ import {
   createApplication,
   disableDebugTools,
   enableDebugTools,
-  getDOM,
   initDomAdapter,
   makeStateKey,
   platformBrowser,
@@ -36,8 +35,11 @@ import {
   provideProtractorTestingSupport,
   withHttpTransferCacheOptions,
   withNoHttpTransferCache
-} from "./chunk-N66RCRT4.js";
-import "./chunk-B5LUOZ3Q.js";
+} from "./chunk-WS7AHM5X.js";
+import {
+  getDOM
+} from "./chunk-SR6J2MIY.js";
+import "./chunk-SFASAAZA.js";
 export {
   BrowserModule,
   By,
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WishlistComponent } from './wishlist/wishlist.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WishlistComponent
+    WishlistComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
