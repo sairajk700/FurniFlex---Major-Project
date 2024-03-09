f1a0c6edbec885700f5c2d6a227f5f079955daad
siddhi <siddhima@cybage.com>
2024-03-09 12:10:09 +0530

    FUR-15 created Product-create form

--- Frontend/src/app/admin/admin.module.ts
+++ Frontend/src/app/admin/admin.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { ProductCreateComponent } from './product-create/product-create.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProductCreateComponent
+  ],
   imports: [
     CommonModule,
     AdminRoutingModule
