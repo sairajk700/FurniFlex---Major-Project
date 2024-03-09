1ed85599a3c79eaa5a7287202111e9345ff63148
Sairaj Khandare <sairajk@cybage.com>
2024-03-09 20:49:41 +0530

    FUR-28-Payment-Page
    Added payment method page.

---
+++ Frontend/src/app/payment-method/payment-method.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-method',
+  templateUrl: './payment-method.component.html',
+  styleUrl: './payment-method.component.css'
+})
+export class PaymentMethodComponent {
+
+}
