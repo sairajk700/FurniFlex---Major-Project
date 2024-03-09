50d8bffc5136168d560021e1f8765cf401b2af3e
siddhi <siddhima@cybage.com>
2024-03-09 12:14:05 +0530

    FUR-15 created Product-edit

--- Frontend/src/app/admin/admin.module.ts
+++ Frontend/src/app/admin/admin.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { ProductCreateComponent } from './product-create/product-create.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
 
 
 @NgModule({
   declarations: [
     ProductCreateComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductEditComponent
   ],
   imports: [
     CommonModule,
