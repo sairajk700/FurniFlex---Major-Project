d6b9ba1740ab10be5d03b41164a421272cd99ac6
siddhi <siddhima@cybage.com>
2024-03-11 17:39:38 +0530

    FUR-23 Created the productlist component

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductpagesComponent } from './productpages/productpages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductpagesComponent
   ],
   imports: [
     BrowserModule,
---
+++ Frontend/src/app/productpages/productpages.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-productpages',
+  templateUrl: './productpages.component.html',
+  styleUrl: './productpages.component.css'
+})
+export class ProductpagesComponent {
+
+}
