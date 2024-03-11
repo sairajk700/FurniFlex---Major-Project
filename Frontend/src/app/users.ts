ed89154bd7b7bda1582b6ba319bbde39d795302a
siddhi <siddhima@cybage.com>
2024-03-11 18:05:44 +0530

    FUR-23 created user Interface

---
+++ Frontend/src/app/users.ts
@@ -0,0 +1,13 @@
+// user.model.ts
+
+export interface User {
+    _id: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    role: string;
+    createdAt: string;
+    updatedAt: string;
+    __v: number;
+  }
+  
